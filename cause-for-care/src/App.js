34891a4464ecb3879d3f6f597d19cc145c53cb80
Riley <mutley1112@gmail.com>
2019-11-10 14:39:16 -0500

    Navigation
    Changes to App.js for navigation purposess

--- cause-for-care/src/App.js
+++ cause-for-care/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./screens/home";
+import User from './screens/userProfile';
+import Organ from './screens/organization';
+import Signup from './screens/signup';
+import Jobapp from './screens/jobApp';
+import Login from './screens/login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router >
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="/">Cause for Care</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/profile">Profile</Nav.Link>
+      <Nav.Link href="/organization">Organization</Nav.Link>
+    </Nav>
+    <Nav>
+      <Button href="/login">Login</Button>
+      <Button variant="success" href="/signup">Signup</Button>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-primary">Search</Button>
+    </Form>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+<hr />
+<Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/profile" component={User} />
+      <Route path="/organization" component={Organ} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/jobapp" component={Jobapp} />
+</Router>
+);
+    }
 
 export default App;
